c8f1c5fb29ee55b742bfca9c9c45650af6fa7921
Hakan Lundvall <hakan.lundvall@gmail.com>
2024-01-02 17:52:48 +0100

    Get artifact id

--- http-push.js
+++ http-push.js
@@ -1,13 +1,29 @@
 const http = require('@actions/http-client');
 let httpc = new http.HttpClient('github-actions');
 (async () => {
-    let headers = { 'Content-Type': 'application/json' };
-    let payload = JSON.stringify({
-        run_id: process.env.GITHUB_RUN_ID,
-        commit_hash: process.env.COMMIT_HASH
-    });
+    let headers = { 'Content-Type': 'application/json',
+                    'authorization': 'Bearer ' + process.env.TEMPLOG_TOKEN };
+    let run_id = process.env.GITHUB_RUN_ID;
+    let commit_hash = process.env.COMMIT_HASH;
+
     console.log(`Pushing ${payload}`);
-    let res = await httpc.post('https://www.lundvall.info/templog/firmware', payload, headers);
+    
+    let res = await httpc.get(`https://api.github.com/repos/hakanlundvall/templog/actions/runs/${run_id}/artifacts`, headers);
+    if (res.message.statusCode != 200) {
+        throw new Error(`Failed to get artifact: ${res.message.statusMessage}`);
+    }
+    let artifacts = JSON.parse(await res.readBody()).artifacts;
+    console.log(`Found ${artifacts.length} artifacts`);
+    console.log(artifacts);
+    
+    headers = { 'Content-Type': 'application/json'}
+ 
+    let payload = JSON.stringify({
+        run_id,
+        commit_hash
+    });
+
+    res = await httpc.post('https://www.lundvall.info/templog/firmware', payload, headers);
     if (res.message.statusCode != 200) {
         throw new Error(`Failed to push: ${res.message.statusMessage}`);
     }

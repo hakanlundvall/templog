93c50ce1d9ab02bcd093ef9d6c0c0edd54cd0dcc
Hakan Lundvall <hakan.lundvall@gmail.com>
2024-01-02 16:45:26 +0100

    debug

--- http-push.js
+++ http-push.js
@@ -1,7 +1,7 @@
 const http = require('@actions/http-client');
 let httpc = new http.HttpClient('github-actions');
 (async () => {
-  let res = await httpc.post('http://www.lundvall.info/templog/firmware', JSON.stringify({run_id: process.env.GITHUB_RUN_ID}));
+  let res = await httpc.post('https://www.lundvall.info/templog/firmware', JSON.stringify({run_id: process.env.GITHUB_RUN_ID}));
   if (res.message.statusCode != 200) {
     throw new Error(`Failed to push: ${res.message.statusMessage}`);
   }

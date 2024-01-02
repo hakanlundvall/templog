be4935981b9a03d50c025d5287cd5ab1c582a050
Hakan Lundvall <hakan.lundvall@gmail.com>
2024-01-02 17:02:34 +0100

    debug

--- http-push.js
+++ http-push.js
@@ -2,7 +2,10 @@ const http = require('@actions/http-client');
 let httpc = new http.HttpClient('github-actions');
 (async () => {
     let headers = { 'Content-Type': 'application/json' };
-    let payload = JSON.stringify({ run_id: process.env.GITHUB_RUN_ID });
+    let payload = JSON.stringify({
+        run_id: process.env.GITHUB_RUN_ID,
+        commit_hash: process.env.COMMIT_HASH
+    });
     console.log(`Pushing ${payload}`);
     let res = await httpc.post('https://www.lundvall.info/templog/firmware', payload, headers);
     if (res.message.statusCode != 200) {
